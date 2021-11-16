85f24517068353971e36a6554393cefbe03f6831
Hadi Syahbal <hadi.syahbal@gmail.com>
2021-11-16 22:20:47 +0800

    install  prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import {useState} from "reaksi";
+import { useState } from 'reaksi';
+import './App.css';
 
-export default function App(){
-    const [count, setCount] = useState(0);
-    const handleCount = (number) => {
-        setCount(count => count + number );
-    }
-    return (
-        <div>
-            <h2>Count : {count}</h2>
-            <div>
-                <button onclick={() => handleCount(1)}>+</button>
-                <button onclick={() => handleCount(-1)}>-</button>
-            </div>
-        </div>
-    );
+export default function App() {
+   const [count, setCount] = useState(0);
+   const handleCount = (number) => {
+      setCount((count) => count + number);
+   };
+   return (
+      <div className='wrapper'>
+         <div className='reaksi-logo'>
+            <img
+               src='https://reaksi.hadi-syahbal.com/images/Reaksi_logo.png'
+               alt='reaksi-logo'
+               width='100%'
+               height='100%'
+            />
+            <h2>Reaksi JS</h2>
+         </div>
+         <h2>Count : {count}</h2>
+         <div>
+            <button onclick={() => handleCount(1)}>+</button>
+            <button onclick={() => handleCount(-1)}>-</button>
+         </div>
+      </div>
+   );
 }
